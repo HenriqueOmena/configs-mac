b613db3a99d75ceaf0128bfe841f96f6164a4742
HenriqueOmena <88lmao@gmail.com>
2025-01-10 12:39:37 +0000

    feat: add option to delete with option + backspack a word

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "<M-BS>", "<C-w>", { desc = "Delete previous word in insert mode" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
